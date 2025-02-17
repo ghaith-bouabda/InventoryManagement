4d276b6e3c564b9c42f45d2298da39ba773c2e37
ghaith <ghaithbouabda@gmail.com>
2025-02-17 12:09:00 +0100

    monolithic approach

---
+++ GestionDuStockBackend/src/main/java/com/pfe/GestioDuStock/GestioDuStockApplication.java
@@ -0,0 +1,13 @@
+package com.pfe.GestioDuStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioDuStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioDuStockApplication.class, args);
+	}
+
+}
--- GestionDuStockBackend/src/test/java/com/pfe/GestioDuStock/GestioDuStockApplicationTests.java
+++ GestionDuStockBackend/src/test/java/com/pfe/GestioDuStock/GestioDuStockApplicationTests.java
@@ -1,10 +1,10 @@
-package com.GestionDustock.configserver;
+package com.pfe.GestioDuStock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class GestioDuStockApplicationTests {
 
 	@Test
 	void contextLoads() {
